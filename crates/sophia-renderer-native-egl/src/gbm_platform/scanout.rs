9cc163cec5b1cd2a234761877aab0faee14ce8f4
Mason Green <mason@greenm01.net>
2026-07-24 12:51:51 -0400

    split native scanout data records

--- crates/sophia-renderer-native-egl/src/gbm_platform/scanout.rs
+++ crates/sophia-renderer-native-egl/src/gbm_platform/scanout.rs
@@ -1,19 +1,22 @@
 use std::{
     ffi::c_void,
-    os::fd::{AsRawFd, BorrowedFd, OwnedFd},
+    os::fd::{AsRawFd, OwnedFd},
     ptr,
     sync::atomic::{AtomicBool, Ordering},
-    time::Duration,
     time::Instant,
 };
+
+mod types;
+
+pub use types::*;
 
 use crate::gbm_platform::{
     EGL_PLATFORM_GBM_KHR,
     config::{window_config_attributes, xrgb_window_config_attributes},
 };
 use crate::gl::{
-    GlCompositionRect, PersistentXrgb8888GlPipeline, context_attributes,
-    draw_xrgb8888_current_gl_context_with_loader, smoke_current_gl_context_with_loader,
+    PersistentXrgb8888GlPipeline, context_attributes, draw_xrgb8888_current_gl_context_with_loader,
+    smoke_current_gl_context_with_loader,
 };
 use crate::{
     NativeGbmRenderedScanoutContextStatus, NativeGbmScanoutBufferExportDetail,
@@ -166,122 +169,6 @@ pub struct NativeGbmRenderedScanoutContext<T: std::os::fd::AsFd> {
     gbm_device: gbm::Device<T>,
     target: Option<PersistentNativeFrameTarget>,
     stats: NativeGbmPersistentRenderStats,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeDmaBufFrame<'a> {
-    pub width: u32,
-    pub height: u32,
-    pub format: u32,
-    pub modifier: u64,
-    pub fd: BorrowedFd<'a>,
-    pub offset: u32,
-    pub stride: u32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeDmaBufPlane<'a> {
-    pub fd: BorrowedFd<'a>,
-    pub offset: u32,
-    pub stride: u32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeMultiPlaneDmaBufFrame<'a> {
-    pub width: u32,
-    pub height: u32,
-    pub format: u32,
-    pub modifier: u64,
-    pub plane_count: u8,
-    pub planes: [Option<NativeDmaBufPlane<'a>>; 4],
-}
-
-impl NativeMultiPlaneDmaBufFrame<'_> {
-    pub fn is_valid(&self) -> bool {
-        const DRM_FORMAT_XRGB8888: u32 = 0x3432_5258;
-        const DRM_FORMAT_ARGB8888: u32 = 0x3432_5241;
-        self.width > 0
-            && self.height > 0
-            && matches!(self.format, DRM_FORMAT_XRGB8888 | DRM_FORMAT_ARGB8888)
-            && self.plane_count > 0
-            && usize::from(self.plane_count) <= self.planes.len()
-            && self.planes[..usize::from(self.plane_count)]
-                .iter()
-                .all(Option::is_some)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct NativeCompositionRect {
-    pub x: i32,
-    pub y: i32,
-    pub width: i32,
-    pub height: i32,
-}
-
-impl From<NativeCompositionRect> for GlCompositionRect {
-    fn from(rect: NativeCompositionRect) -> Self {
-        Self {
-            x: rect.x,
-            y: rect.y,
-            width: rect.width,
-            height: rect.height,
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeCpuCompositionLayer<'a> {
-    pub width: u32,
-    pub height: u32,
-    pub stride: u32,
-    pub format: u32,
-    pub pixels: &'a [u8],
-    pub target: NativeCompositionRect,
-    pub clip: Option<NativeCompositionRect>,
-    pub alpha: f32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeDmaBufCompositionLayer<'a> {
-    pub frame: NativeMultiPlaneDmaBufFrame<'a>,
-    pub target: NativeCompositionRect,
-    pub clip: Option<NativeCompositionRect>,
-    pub alpha: f32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub enum NativeCompositionLayer<'a> {
-    Cpu(NativeCpuCompositionLayer<'a>),
-    DmaBuf(NativeDmaBufCompositionLayer<'a>),
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct NativeCompositionFrame<'a> {
-    pub width: u32,
-    pub height: u32,
-    pub layers: &'a [NativeCompositionLayer<'a>],
-}
-
-impl NativeDmaBufFrame<'_> {
-    pub fn is_valid(&self) -> bool {
-        const DRM_FORMAT_XRGB8888: u32 = 0x3432_5258;
-        const DRM_FORMAT_ARGB8888: u32 = 0x3432_5241;
-        self.width > 0
-            && self.height > 0
-            && matches!(self.format, DRM_FORMAT_XRGB8888 | DRM_FORMAT_ARGB8888)
-            && self.stride >= self.width.saturating_mul(4)
-            && matches!(self.modifier, 0 | u64::MAX)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
-pub struct NativeGbmPersistentRenderStats {
-    pub target_creations: usize,
-    pub target_recreations: usize,
-    pub gl_pipeline_creations: usize,
-    pub frame_uploads: usize,
-    pub max_upload: Duration,
 }
 
 struct PersistentNativeFrameTarget {
